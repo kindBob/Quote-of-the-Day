f2105c42b28d7da49721d59affb0ac8807783062
kindBob <asdyrtveflmh@gmail.com>
2023-09-24 23:03:02 +0200

    fixed flip card tap recognition

--- scripts/userInteractions.js
+++ scripts/userInteractions.js
@@ -1,20 +1,27 @@
 const navBarList = document.querySelector(".nav-bar__list");
 const burgerMenu = document.querySelector(".nav-bar__burger-menu");
+const quoteElement = document.querySelector(".quotes-element");
+
+quoteElement.addEventListener("click", (e) => {
+  quoteElement.classList.toggle("--flipped");
+});
 
 burgerMenu.addEventListener("click", () => {
   navBarList.classList.toggle("--active");
   burgerMenu.classList.toggle("--active");
 
-  navBarList.classList.contains("--active") ? document.body.style.overflow = "hidden" : document.body.style.overflow = "auto";
+  navBarList.classList.contains("--active")
+    ? (document.body.style.overflow = "hidden")
+    : (document.body.style.overflow = "auto");
 });
 
-for(let i = 0; i < navBarList.children.length; i++){
+for (let i = 0; i < navBarList.children.length; i++) {
   navBarList.children[i].addEventListener("click", () => {
-    closeNavBarList()
-  })
+    closeNavBarList();
+  });
 }
 
-function closeNavBarList(){
+function closeNavBarList() {
   console.log(1);
   navBarList.classList.remove("--active");
   burgerMenu.classList.remove("--active");
