7e8e4538d201f443ec5b5444521a639434865af0
kindBob <asdyrtveflmh@gmail.com>
2023-09-25 21:05:54 +0200

    sharing quotes-card/v.0.3

--- scripts/quotesManager.js
+++ scripts/quotesManager.js
@@ -6,10 +6,11 @@ const dateOuputs = document.querySelectorAll(".quotes-element__date");
 const quoteOutput = document.querySelectorAll(".quotes-element__quote");
 const authorOutput = document.querySelectorAll(".quotes-element__author");
 
+const lineBreak = document.createElement("br");
+
 dateOuputs[0].innerHTML = TimeMachine.currentDateFormatted;
 dateOuputs[1].innerHTML = TimeMachine.tomorrowsDateFormatted;
 dateOuputs[2].innerHTML = TimeMachine.afterTomorrowsDayFormatted;
-dateOuputs[dateOuputs.length - 1].innerHTML = TimeMachine.currentDateFormatted;
 
 const quoteAuthorRegex = /(\S+)\s+([a-z]{3,})/i;
 
@@ -20,11 +21,13 @@ if (localStorage.getItem("currentQuote") == null || JSON.parse(localStorage.getI
       authorOutput[i].innerHTML = quoteObject.author.replace(quoteAuthorRegex, "$1<br>$2");
     }
 
+    
+
     localStorage.setItem("currentQuote", JSON.stringify(quoteObject));
   });
 } else {
   let quoteObject = JSON.parse(localStorage.getItem("currentQuote"));
-
+  
   for(let i = 0; i < quoteOutput.length; i++){
     quoteOutput[i].innerHTML = quoteObject.quote;
     authorOutput[i].innerHTML = quoteObject.author.replace(quoteAuthorRegex, "$1<br>$2");
